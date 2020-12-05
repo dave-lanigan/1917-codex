ba52380b9d0c05b5140c7599ab55449597914d8d
Dáithí <dav.lanigan@gmail.com>
2020-12-05 15:55:59 -0500

    Update settings.py

--- codex/codex/settings.py
+++ codex/codex/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = conf["secret-key"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = conf["allowed-hosts"]
+#ALLOWED_HOSTS = conf["allowed-hosts"]
+ALLOWED_HOSTS = conf["*"]
 
 
 # Application definition

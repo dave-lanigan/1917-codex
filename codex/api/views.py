e1863bd677a735911ff1741881cee5c9b25b2b82
David M.J. Lanigan <dav.lanigan@gmail.com>
2020-12-05 18:15:52 -0500

    fix: cors

--- codex/api/views.py
+++ codex/api/views.py
@@ -57,8 +57,8 @@ def content_book(request, book_num):
         if book.find(book_num) != -1:
 
             resp = JsonResponse(list(coll.find())[0]["book"][book], safe=False)
-            #resp['Access-Control-Allow-Origin'] = "http://35.243.203.173"
-            resp['Access-Control-Allow-Origin'] = "*"
+            resp['Access-Control-Allow-Origin'] = "http://35.243.203.173"
+            #resp['Access-Control-Allow-Origin'] = "*"
             return resp
 
 # def content_book(request, book_num, part_num, section_num):

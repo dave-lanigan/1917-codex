b20bd88d70af2c924845837ecbb8fa9c53de6b4a
David M.J. Lanigan <dav.lanigan@gmail.com>
2020-12-06 15:52:59 -0500

    update: cors issue

--- codex/fcontent/src/components/App.js
+++ codex/fcontent/src/components/App.js
@@ -16,12 +16,13 @@ export default function App(props) {
     console.log("fucking here 1.");
     //let url="https://cors-anywhere.herokuapp.com/http://iudicabit.mywire.org/api/v0/codex/book-1/";
     let url="http://iudicabit.mywire.org/api/v0/codex/book-1/";
+    //let url = "https://api.thecatapi.com/v1/images/search";
     //let url="http://localhost:8000/api/v0/codex/book-1/";
     //let url="http://35.243.203.173/api/v0/codex/book-2/";
     //let url="/api/v0/codex/book-2/";
     //let url = CONF["book1"]
     console.log("fucking here 2.");
-    return fetch( url )
+    fetch( url )
     .then( (resp) => resp.json())
     .then( (json) =>{ 
       setData( {loading: false, data: json } )

5af109df3b9eab300d672674f5cd868c6e0c288c
David M.J. Lanigan <dav.lanigan@gmail.com>
2020-12-06 15:30:36 -0500

    update: net issue

--- codex/fcontent/src/components/App.js
+++ codex/fcontent/src/components/App.js
@@ -13,14 +13,14 @@ export default function App(props) {
 
 
   function ggetData() {
-    
+    console.log("fucking here 1.");
     //let url="https://cors-anywhere.herokuapp.com/http://iudicabit.mywire.org/api/v0/codex/book-1/";
     let url="http://iudicabit.mywire.org/api/v0/codex/book-1/";
     //let url="http://localhost:8000/api/v0/codex/book-1/";
     //let url="http://35.243.203.173/api/v0/codex/book-2/";
     //let url="/api/v0/codex/book-2/";
     //let url = CONF["book1"]
-    console.log("fucking here.");
+    console.log("fucking here 2.");
     return fetch( url )
     .then( (resp) => resp.json())
     .then( (json) =>{ 

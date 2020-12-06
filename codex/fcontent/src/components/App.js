c84396559a1aebb98a5251d0d4870499ee9ada86
David M.J. Lanigan <dav.lanigan@gmail.com>
2020-12-06 07:47:31 -0500

    fix: cors issue

--- codex/fcontent/src/components/App.js
+++ codex/fcontent/src/components/App.js
@@ -15,10 +15,10 @@ export default function App(props) {
   function ggetData() {
     
     //let url="https://cors-anywhere.herokuapp.com/http://iudicabit.mywire.org/api/v0/codex/book-1/";
-    let url="http://iudicabit.mywire.org/api/v0/codex/book-1/";
+    //let url="http://iudicabit.mywire.org/api/v0/codex/book-1/";
     //let url="http://localhost:8000/api/v0/codex/book-1/";
     //let url="http://35.243.203.173/api/v0/codex/book-2/";
-    //let url="/api/v0/codex/book-2/";
+    let url="/api/v0/codex/book-2/";
     //let url = CONF["book1"]
 
     return fetch( url ).then( (resp) => resp.json()).then( (json) =>{ 
